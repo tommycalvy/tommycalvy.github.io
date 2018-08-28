b4c582d4a474fab150b6a9681856df075e9af247
Tommy Calvy <thomaslcalvy@gmail.com>
2018-08-28 18:50:09 -0400

    Functional Header
    Made the header functional.

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -1,1 +1,9 @@
-window.sr=ScrollReveal();
+function showNav(){
+  console.log("This is working");
+  var x = document.getElementById("responsive-nav");
+  if (x.className === "responsive-nav") {
+    x.className += " unfold";
+  } else {
+    x.className = "responsive-nav";
+  }
+}
